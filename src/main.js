7ab1b3dabb0b7e63211fce7d96b472daf7c1774a
Alexyang66 <hljcy1997@126.com>
2020-12-31 13:35:58 +0800

    web-cy1.0

--- src/main.js
+++ src/main.js
@@ -4,6 +4,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+
+
+import axios from 'axios';
+axios.defaults.baseURL='http://localhost:8087/';
+Vue.prototype.axios = axios;
+
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import login from '@/components/login'
+
+import Homes from '@/components/Homes'
+import welcome from '@/components/welcome'
+
 
 Vue.use(Router)
 
@@ -8,8 +12,20 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'login',
+      component: login
+    },
+    {
+      path:'/Homes',
+      name:'Homes',
+      component:Homes,
+      redirect:"/welcome",
+      children:[
+        {path:'/welcome',component:welcome},
+
+
+      ]
     }
+
   ]
 })
